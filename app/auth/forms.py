0cf1767d5ecae3b7b1ede7566d325188b0533f1a
blancc-page <blancc.666@gmail.com>
2022-05-17 22:58:08 +0300

    feat[auth]: accept incoming changes

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-from email import message
-from xml.dom import ValidationErr
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, ValidationError
-from wtforms.validators import DataRequired, Email, EqualTo
-from ..models import User
-
-class RegistrationForm(FlaskForm):
-    firstname = StringField("First Name", validators = [DataRequired()])
-    middlename = StringField("Middle Name")
-    lastname = StringField("Last Name", validators = [DataRequired()])
-    email= StringField("Email", validators=[DataRequired()])
-    phonenumber = StringField("Phone Number", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired(), EqualTo("password_confirm", message="Passwords did not match")])
-    password_confirm = PasswordField("Confirm Password", validators=[DataRequired()])
-    submit = SubmitField("Register")
-    
-    def validate_email(self, email_field):
-        if User.query.filter_by(email = email_field.data).first():
-            raise ValidationError("Account Exists")
-=======
 from flask_wtf import FlaskForm 
 from wtforms import StringField,SubmitField,PasswordField,ValidationError,BooleanField
 from wtforms.validators import DataRequired,Email,EqualTo
@@ -47,4 +25,3 @@ class LoginForm(FlaskForm):
   submit = SubmitField('Login')
     
   
->>>>>>> 0dc11873856e28e5a8ad487e9936abc8eb600c14
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-from pickle import GET
-from turtle import title
-from flask import render_template, redirect, url_for, flash , request
-from . import auth
-from .. models import *
-from .forms import RegistrationForm
-from .. import db
-
-
-@auth.route("/register", methods = ['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    title = 'Registration'
-    return render_template('auth/register.html', form = form, title = title)
-=======
 from flask import render_template,redirect,url_for,request,flash
 from . import auth
 from ..models import *
@@ -44,4 +28,3 @@ def login():
   title = 'Login'  
   return render_template('auth/login.html', form=form,title=title)
     
->>>>>>> 0dc11873856e28e5a8ad487e9936abc8eb600c14
