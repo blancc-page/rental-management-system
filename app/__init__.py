d7a08fe0b4d08b97930ab2603d287492b18a101b
Collins Sirwani <sircollo71@gmail.com>
2022-05-20 09:34:58 +0300

    Edit secret keys and configuration settings

--- app/__init__.py
+++ app/__init__.py
@@ -27,7 +27,7 @@ def create_app(config_name):
   app.config.from_object(config_options[config_name])
  
   db.init_app(app)
-  app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://eamvujmgmwjvxz:283e508ab9d77949f23009cafe38285920d6c0220d2975b17f56b18c11b7b1bc@ec2-34-201-95-176.compute-1.amazonaws.com:5432/d6bvv8hvt30f8e"
+  
   
   #initialize flask extensions
   bootstrap.init_app(app)
