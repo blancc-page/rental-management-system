2a03a5a6e91fa5f1131fe23fab9d2da1c5c28e2c
Collins Sirwani <sircollo71@gmail.com>
2022-05-20 08:41:28 +0300

    Congigure database uri

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
 
 class ProdConfig(Config):
   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
+  DATABASE_URL="postgresql://eamvujmgmwjvxz:283e508ab9d77949f23009cafe38285920d6c0220d2975b17f56b18c11b7b1bc@ec2-34-201-95-176.compute-1.amazonaws.com:5432/d6bvv8hvt30f8e"
 class TestConfig(Config):
   pass
 
